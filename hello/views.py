4cdcbd57fd7d4e0de95aaf73449d1218da7fdf8e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-17 12:53:54 +0200

    aws

--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 import os
 from django.shortcuts import render
 
-ENV = os.environ.get('ENV', 'dev')
+ENV = os.environ.get('ENV', 'dev-test')
 
 
 def hello_world(request):
-    return HttpResponse(f"<h1>Hello, World!</h1><p>Środowisko: {ENV}</p><p>Autor: <b>Karolina Szymaszkiewicz</b></p>")
+    return HttpResponse(f"<h1>Hello, World!</h1><p>Środowisko: {ENV}</p><p>Autor: <b>Karolina Szymaszkiewicz - webszyk</b></p>")
