a237d28759a76209376689c2370215b88374c2d6
KarlaSz <szymaszkiewiczk@gmail.com>
2025-05-18 07:02:22 +0200

    aws deploy

--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,12 @@
 from django.test import SimpleTestCase
 
+from hello.views import ENV
+
+
 class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, second=200)
     def test_home_page_contains_correct_html(self):
         response = self.client.get('/')
-        self.assertContains(response, 'Hello, django app!')
+        self.assertContains(response, f"<h1>Hello, django app!</h1><p>Środowisko: {ENV}</p><p>Autor: <b>Karolina Szymaszkiewicz - webszyk</b></p>")
