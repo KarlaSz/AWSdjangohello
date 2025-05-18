3b9dad162ad44cfff529ad50043dcbd18d6cb78d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-18 10:46:29 +0200

    aws db deploy

--- djangohello/settings.py
+++ djangohello/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-ENV = os.environ.get('ENV', 'dev-test')
+ENV = os.environ.get('ENV', 'dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
